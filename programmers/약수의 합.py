94ff835cdfa2c74d8cda251cf973a53fac36b07e
YEOSEOJEONG <121150215+yeoseojeong@users.noreply.github.com>
2024-06-13 09:36:07 +0900

    Create 약수의 합.py

---
+++ programmers/약수의 합.py
@@ -0,0 +1,11 @@
+def solution(n):
+    answer = 0
+    total=0
+    
+    for i in range(1,n+1):
+        if n%i==0:
+            total+=i
+        else:
+            total+=0
+        
+    return total
