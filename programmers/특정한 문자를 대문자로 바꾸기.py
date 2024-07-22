0f0036dd7b1543dd090bb830bcfdebf82a45075e
YEOSEOJEONG <121150215+yeoseojeong@users.noreply.github.com>
2024-07-22 17:20:06 +0900

    Create 특정한 문자를 대문자로 바꾸기.py

---
+++ programmers/특정한 문자를 대문자로 바꾸기.py
@@ -0,0 +1,10 @@
+def solution(my_string, alp):
+    answer = ''
+    
+    for i in my_string:
+        if i == alp:
+            answer += i.upper()  
+        else:
+            answer += i  
+            
+    return answer
