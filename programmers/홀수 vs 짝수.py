b815c4d200c828a4712ccc78c82076348c636913
YEOSEOJEONG <121150215+yeoseojeong@users.noreply.github.com>
2024-08-20 16:36:41 +0900

    Create 홀수 vs 짝수.py

---
+++ programmers/홀수 vs 짝수.py
@@ -0,0 +1,16 @@
+def solution(num_list):
+    answer = 0
+    even=0
+    odd=0
+    
+    for i in range(len(num_list)):
+        if i%2==0:
+            even+=num_list[i]
+        else:
+            odd+=num_list[i]
+    
+    
+    if even>odd:
+        return even
+    else:
+        return odd
