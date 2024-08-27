d50971985642386276cd12fa3c2cd7ec23cc778e
YEOSEOJEONG <121150215+yeoseojeong@users.noreply.github.com>
2024-08-27 16:35:34 +0900

    Create 순서 바꾸기.py

---
+++ programmers/순서 바꾸기.py
@@ -0,0 +1,9 @@
+def solution(num_list, n):
+    answer = []
+
+    for i in range(n, len(num_list)):
+        answer.append(num_list[i])
+    
+    for j in range(0,n):
+        answer.append(num_list[j])
+    return answer
