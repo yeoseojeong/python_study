29ae4004295ebd2c223f19cc45ad16fb00ce12ce
YEOSEOJEONG <121150215+yeoseojeong@users.noreply.github.com>
2024-12-02 16:32:53 +0900

    Create 간단한 식 계산하기.py

---
+++ programmers/간단한 식 계산하기.py
@@ -0,0 +1,10 @@
+def solution(binomial):
+    a, op, b = binomial.split()
+    if op == '+': 
+        return int(a) + int(b)
+    if op == '-': 
+        return int(a) - int(b)
+    if op == '*': 
+        return int(a) * int(b)
+    if op == '/': 
+        return int(a) / int(b)
